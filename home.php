cfd287a928dc7f87794327e324b044c596a31b11
Novan Nur Zulhilmi Yardana <novan.zulhilmi2199@gmail.com>
2024-05-11 21:55:25 +0700

    Upload CRUD MAPEL

--- home.php
+++ home.php
@@ -1,10 +1,11 @@
 <div class="card">
     <div class="card-body">
         <p>
-            <img src="gambar/code.jfif" class="rounded float-end" alt="WebDev" width="500px">
-            <b><i>Programming Website</i></b> adalah proses mengkomunikasikan perintah pada komputer untuk membangun suatu program berbasis website agar dapat diakses menggunakan internet.
+            <img src="gambar/code.jpg" class="rounded float-end" alt="WebDev" width="500px">
+            <b><i>Programming Website</i></b> adalah proses yang melibatkan penggunaan bahasa pemrograman dan teknologi web untuk mengkomunikasikan perintah-perintah kepada komputer, dengan tujuan membangun suatu program atau aplikasi berbasis website yang dapat diakses melalui internet. 
             <br>
-            <b>Student Day Web Programming</b> adalah salah satu Student Day yang ada di SMA Plus PGRI Cibinong yang mempersiapkan siswa/i mendapatkan keterampilan dalam bilang Programming.
+            <br>
+            <b>Student Day Web Programming</b> adalah salah satu acara yang diselenggarakan di SMA Plus PGRI Cibinong, bertujuan untuk mempersiapkan siswa dan siswi agar memperoleh keterampilan dalam bidang pemrograman, khususnya dalam konteks pengembangan website dan aplikasi web.
         </p>
     </div>
 </div>
